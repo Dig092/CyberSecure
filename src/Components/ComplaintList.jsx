1c45ca2c9a97783dfab32ca9be9040e4f2753776
Dhruval Gupta <diggupta092@gmail.com>
2024-01-15 19:00:03 +0530

    updated Dashboard

--- src/Components/ComplaintList.jsx
+++ src/Components/ComplaintList.jsx
@@ -1,48 +1,34 @@
+
 import PropTypes from "prop-types";
 
 const ComplaintList = ({ complaints }) => {
   return (
     <div>
       <h1>Complaint List</h1>
-      <div>
+      <ul>
         {complaints.map((complaint) => (
-          <div key={complaint._id}>
-            <p>User: {complaint.user.name}</p>
-            <p>Category: {complaint.category}</p>
-            <p>Subcategory: {complaint.subcategory}</p>
-            <p>Date: {complaint.date}</p>
-            <p>Time: {complaint.time}</p>
+          <li key={complaint._id}>
+            <p>Name: {complaint.user.name}</p>
             <p>District: {complaint.district}</p>
             <p>State: {complaint.state}</p>
-            {/* Add more fields as needed */}
-            <p>Action Taken: {complaint.actionTaken}</p>
-            <p>Verification Status: {complaint.verificationStatus ? 'Verified' : 'Not Verified'}</p>
-            {/* Add more details as needed */}
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   );
 };
 
 ComplaintList.propTypes = {
-    complaints: PropTypes.arrayOf(
-      PropTypes.shape({
-        _id: PropTypes.string.isRequired,
-        user: PropTypes.shape({
-          name: PropTypes.string.isRequired,
-        }).isRequired,
-        category: PropTypes.string.isRequired,
-        subcategory: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-        time: PropTypes.string.isRequired,
-        district: PropTypes.string.isRequired,
-        state: PropTypes.string.isRequired,
-        actionTaken: PropTypes.string.isRequired,
-        verificationStatus: PropTypes.bool.isRequired,
-        // Add more propTypes as needed
-      })
-    ).isRequired,
-  };
+  complaints: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      user: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+      }).isRequired,
+      district: PropTypes.string.isRequired,
+      state: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
 
 export default ComplaintList;
