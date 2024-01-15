c6902b1145e87082881f84b3794127d8644fb65e
Dhruval Gupta <diggupta092@gmail.com>
2024-01-15 19:04:11 +0530

    updated Dashboard

--- src/Components/ComplaintList.jsx
+++ src/Components/ComplaintList.jsx
@@ -1,5 +1,5 @@
-
 import PropTypes from "prop-types";
+import user from "../assets/images/UserLogin.png"
 
 const ComplaintList = ({ complaints }) => {
   return (
@@ -7,10 +7,27 @@ const ComplaintList = ({ complaints }) => {
       <h1>Complaint List</h1>
       <ul>
         {complaints.map((complaint) => (
-          <li key={complaint._id}>
-            <p>Name: {complaint.user.name}</p>
-            <p>District: {complaint.district}</p>
-            <p>State: {complaint.state}</p>
+          <li
+            key={complaint._id}
+            className="m-4 rounded px-4 py-2"
+            style={{
+              boxShadow: "-4px 4px 12px 0px rgba(0, 0, 0, 0.06)",
+            }}
+          >
+            <div className="flex items-center justify-between gap-4">
+              <div className="flex items-center gap-4">
+                <img className="w-12" src={user} alt="" />
+                <div className="font-semibold">
+                  <h1>{complaint.user.name}</h1>
+                  <h1>
+                    {complaint.district}, {complaint.state}
+                  </h1>
+                </div>
+              </div>
+              <button className="bg-blue-500 px-8 py-2 rounded-md text-white">
+                View
+              </button>
+            </div>
           </li>
         ))}
       </ul>
