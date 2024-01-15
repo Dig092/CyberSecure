494bde75f6bc807378ebc1a59c4fad295f315e33
Dhruval Gupta <diggupta092@gmail.com>
2024-01-15 22:58:59 +0530

    updated ComplaintList

--- src/Components/ComplaintDetails.jsx
+++ src/Components/ComplaintDetails.jsx
@@ -1,18 +1,45 @@
 import PropTypes from "prop-types";
 
 const ComplaintDetails = ({ selectedComplaint, onClose }) => {
-    console.log("Selected Complaint:", selectedComplaint);
+  console.log("Selected Complaint:", selectedComplaint);
+
   return (
-    <div className="selected-complaint-details">
-      <h2>Details for {selectedComplaint.user.name}</h2>
-      <p>Category: {selectedComplaint.category}</p>
-      <p>Subcategory: {selectedComplaint.subcategory}</p>
-      <p>Date: {selectedComplaint.date}</p>
-      <p>Time: {selectedComplaint.time}</p>
-      <p>District: {selectedComplaint.district}</p>
-      <p>State: {selectedComplaint.state}</p>
-      {/* Add more details as needed */}
-      <button onClick={onClose}>Close Details</button>
+    <div className="fixed inset-0 overflow-y-auto p-4 bg-gray-800 bg-opacity-75 z-50">
+      <div className="max-w-md mx-auto bg-white rounded p-6">
+        <h2 className="text-2xl font-semibold mb-4">
+          Details for {selectedComplaint.user.name}
+        </h2>
+        <div className="mb-2">
+          <p>
+            <span className="font-bold">Category:</span>{" "}
+            {selectedComplaint.category}
+          </p>
+          <p>
+            <span className="font-bold">Subcategory:</span>{" "}
+            {selectedComplaint.subcategory}
+          </p>
+          <p>
+            <span className="font-bold">Date:</span> {selectedComplaint.date}
+          </p>
+          <p>
+            <span className="font-bold">Time:</span> {selectedComplaint.time}
+          </p>
+          <p>
+            <span className="font-bold">District:</span>{" "}
+            {selectedComplaint.district}
+          </p>
+          <p>
+            <span className="font-bold">State:</span> {selectedComplaint.state}
+          </p>
+          {/* Add more details as needed */}
+        </div>
+        <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-full hover:bg-blue-600"
+          onClick={onClose}
+        >
+          Close Details
+        </button>
+      </div>
     </div>
   );
 };
