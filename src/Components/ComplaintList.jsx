9bf88e4f2aaff8e6a059bc3a58e5f9464c4425cc
Dhruval Gupta <diggupta092@gmail.com>
2024-01-18 10:24:16 +0530

    change-10

--- src/Components/ComplaintList.jsx
+++ src/Components/ComplaintList.jsx
@@ -28,45 +28,44 @@ const ComplaintList = ({ complaints, onVerify }) => {
 
   return (
     <div style={styles.container} className="complaint-list-container">
-      <ul className="complaint-list">
-        {complaints.map((complaint) => (
-          <li
-            key={complaint._id}
-            className="m-4 rounded px-4 py-2"
-            style={{
-              boxShadow: "-4px 4px 12px 0px rgba(0, 0, 0, 0.06)",
-            }}
-          >
-            <div className="flex items-center justify-between gap-4">
-              <div className="flex items-center gap-4">
-                <img className="w-12" src={user} alt="" />
-                <div className="font-semibold">
-                  <h1>{complaint.user.name}</h1>
-                  <h1>
-                    {complaint.district}, {complaint.state}
-                  </h1>
-                </div>
-              </div>
-              <button
-                className="bg-blue-500 px-8 py-2 rounded-md text-white"
-                onClick={() => handleViewClick(complaint)}
-              >
-                View
-              </button>
+  <ul className="complaint-list">
+    {complaints.map((complaint) => (
+      <li
+        key={complaint._id}
+        className="m-4 rounded px-4 py-2"
+        style={{
+          boxShadow: "-4px 4px 12px 0px rgba(0, 0, 0, 0.06)",
+        }}
+      >
+        <div className="flex items-center justify-between gap-4">
+          <div className="flex items-center gap-4">
+            <img className="w-12" src={user} alt="" />
+            <div className="font-semibold">
+              <h1>{complaint.user && complaint.user.name}</h1>
+              <h1>
+                {complaint.district}, {complaint.state}
+              </h1>
             </div>
-          </li>
-        ))}
-      </ul>
-      {selectedComplaint && (
-        <ComplaintDetails
-          selectedComplaint={selectedComplaint}
-          onClose={handleCloseDetails}
-          onVerify={onVerify} 
-        />
-      )}
-    </div>
-  );
-};
+          </div>
+          <button
+            className="bg-blue-500 px-8 py-2 rounded-md text-white"
+            onClick={() => handleViewClick(complaint)}
+          >
+            View
+          </button>
+        </div>
+      </li>
+    ))}
+  </ul>
+  {selectedComplaint && (
+    <ComplaintDetails
+      selectedComplaint={selectedComplaint}
+      onClose={handleCloseDetails}
+      onVerify={onVerify} 
+    />
+  )}
+</div>
+
 
 ComplaintList.propTypes = {
   complaints: PropTypes.arrayOf(
