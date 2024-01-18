c5eb09c0bc7e7bcd9859587010bff7412de11121
Dhruval Gupta <diggupta092@gmail.com>
2024-01-18 09:45:27 +0530

    change-6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Suspense } from "react";
-import { I18nextProvider } from 'react-i18next';
-import i18next from './i18n';
+// import { Suspense } from "react";
+// import { I18nextProvider } from 'react-i18next';
+// import i18next from './i18n';
 
 import HomePage from "./Homepage";
 import AdminLogin from "./Pages/AdminLogin";
@@ -11,8 +11,8 @@ import Menu from "./Components/Menu";
 
 const App = () => {
   return (
-    <I18nextProvider i18n={i18next}>
-      <Suspense fallback="loading">
+    // <I18nextProvider i18n={i18next}>
+      // <Suspense fallback="loading">
         <Router>
           <Menu />
           <Routes>
@@ -22,8 +22,8 @@ const App = () => {
             <Route path="/Dashboard" element={<Dashboard />} />
           </Routes>
         </Router>
-      </Suspense>
-    </I18nextProvider>
+      // </Suspense>
+    // </I18nextProvider>
   );
 };
 
--- src/i18n.js
+++ src/i18n.js
@@ -3,22 +3,22 @@
 // import Backend from 'i18next-http-backend';
 // import LanguageDetector from 'i18next-browser-languagedetector';
 
-// // i18next
-// //   .use(Backend)
-// //   .use(LanguageDetector)
-// //   .use(initReactI18next)
-// //   .init({
-// //     fallbackLng: 'en', // Default language
-// //     debug: true,
-// //     resources: {
-// //       en: {
-// //         translation: require('./public/locales/en/translations.json'),
-// //       },
-// //       fr: {
-// //         translation: require('./public/locales/fr/translations.json'),
-// //       },
-// //       // Add other languages as needed
-// //     },
-// //   });
+// i18next
+//   .use(Backend)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     fallbackLng: 'en', // Default language
+//     debug: true,
+//     resources: {
+//       en: {
+//         translation: require('./public/locales/en/translations.json'),
+//       },
+//       fr: {
+//         translation: require('./public/locales/fr/translations.json'),
+//       },
+//       // Add other languages as needed
+//     },
+//   });
 
 // export default i18next;
